8b844ae359ba4c1dd5ea7e572adea33d236706c5
kozyrev-dv <k-daniilv@mail.ru>
2023-06-01 12:35:48 +0300

    woops

--- src/moks/offers-by-type-moks.js
+++ src/moks/offers-by-type-moks.js
@@ -1,6 +1,3 @@
-import { randomInt, randomString } from '../framework/utils/random-utils';
-import { OFFER_TYPES } from './const';
-
 const createOffer = (id, title, price) => ({
   'id' : id,
   'title' : title,
