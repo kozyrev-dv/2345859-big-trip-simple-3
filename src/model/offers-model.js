6eb3cafea615ef10eb32cd4c0e6054dffa81c868
kozyrev-dv <k-daniilv@mail.ru>
2023-06-10 17:23:06 +0300

    [WIP] offers model loads from server

--- src/model/offers-model.js
+++ src/model/offers-model.js
@@ -1,22 +1,24 @@
-import { EVENT_TYPES } from '../moks/const';
-import { randomInt, randomString } from '../framework/utils/random-utils';
-import { createOffer } from '../moks/offers-by-type-moks';
+import { UpdateType } from '../moks/const';
+import Observable from '../framework/observable';
 
-export default class OffersModel {
+export default class OffersModel extends Observable{
 
+  #tripPointApiService = null;
   #offersByType = [];
 
-  constructor() {
-    this.#offersByType = Array.from(EVENT_TYPES, (type) => {
-      const offers = Array.from(
-        {length: randomInt(5, 2)},
-        (_, index) => createOffer(index, randomString(10), randomInt(100, 10))
-      );
-      return {
-        'type': type,
-        'offers': offers
-      };
-    });
+  constructor(tripPointApiService) {
+    super();
+    this.#tripPointApiService = tripPointApiService;
+    // this.#offersByType = Array.from(EVENT_TYPES, (type) => {
+    //   const offers = Array.from(
+    //     {length: randomInt(5, 2)},
+    //     (_, index) => createOffer(index, randomString(10), randomInt(100, 10))
+    //   );
+    //   return {
+    //     'type': type,
+    //     'offers': offers
+    //   };
+    // });
   }
 
   getOffersOfType = (type) => this.#offersByType.find((el) => el.type === type).offers;
@@ -24,4 +26,16 @@ export default class OffersModel {
   get offersByType() {
     return this.#offersByType;
   }
+
+  init = async () => {
+
+    try {
+      this.#offersByType = await this.#tripPointApiService.offers;
+    } catch(err) {
+      this.#offersByType = [];
+    }
+    this._notify(UpdateType.INIT);
+
+  };
+
 }
